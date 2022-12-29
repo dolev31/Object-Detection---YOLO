970cba4126b085c5160213a3dbde27222e85ac8e
dolev31 <102147552+dolev31@users.noreply.github.com>
2022-12-29 23:05:14 +0200

    Delete train.py

--- train.py
+++
@@ -1,7 +0,0 @@
-
-# !pip install roboflow
-
-from roboflow import Roboflow
-rf = Roboflow(api_key="UKAne430cjV8yrABlkf8")
-project = rf.workspace("technion-vb9eo").project("object_detection_sergical_tools")
-dataset = project.version(1).download("yolov5")
