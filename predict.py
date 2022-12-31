b6f2f986249d03bbeb36a502fa0c0ca343d3a8ad
dn282 <dn2828@gmail.com>
2022-12-31 17:59:30 +0200

    adding requirements

--- predict.py
+++ predict.py
@@ -5,10 +5,10 @@ from segment_utils.segment import single_segmentation, single_img_annotation
 
 
 def segment_img(
-        img_file_path,
+        image_file_name,
         model_path
 ):
-    img_name = ".".join(img_file_path.split("/")[-1].split(".")[0:-1])
+    img_name = image_file_name.split(".")[0]
     result_path = r"{}".format(f"results/{img_name}.jpg")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -17,7 +17,7 @@ def segment_img(
     model.max_det = 2
     model.amp = True
 
-    img = cv2.imread(img_file_path)
+    img = cv2.imread("images/" + image_file_name)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     frame_res = model(img)
     pred, right_label, left_label = single_segmentation(frame_res)
@@ -29,16 +29,16 @@ def segment_img(
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--img_file_path', help='path of image to annotate')
+    parser.add_argument('--image_file_name', help='path of image to annotate')
     parser.add_argument('--model_path', help='path to pretrained yolo model')
 
     args = parser.parse_args()
-    return args.img_file_path, args.model_path
+    return args.image_file_name, args.model_path
 
 
 def main():
-    img_file_path, model_path = parse_args()
-    segment_img(img_file_path, model_path)
+    image_file_name, model_path = parse_args()
+    segment_img(image_file_name, model_path)
 
 
 if __name__ == "__main__":
